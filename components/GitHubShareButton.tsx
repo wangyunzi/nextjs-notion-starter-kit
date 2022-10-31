d40bad2eaae4bb490d2aab4959865f4a5fe9c17c
王云子 <85829302+wangyunzi@users.noreply.github.com>
2022-10-31 16:48:44 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/wangyunzi/nextjs-notion-starter-kit'
+      href='https://github.com/wangyunzi/yazi'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
